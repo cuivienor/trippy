292e0cf3575bba681f4574a1ddeba711fd707d97
James Leisy <jamesleisy@gmail.com>
2015-08-30 12:58:47 -0400

    do not push

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+resources :users do
+  resources :locations do
+    resources :pois
+    resources :trips
+  end
+end
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
