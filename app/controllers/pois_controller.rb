109abf7b26c8f2b47b2b209114071823749f4609
James Leisy <jamesleisy@gmail.com>
2015-08-30 15:54:43 -0400

    don't commit

--- app/controllers/pois_controller.rb
+++ app/controllers/pois_controller.rb
@@ -1,7 +1,11 @@
 class PoisController < ApplicationController
 
 	def index
-		
+		@pois = Poi.all
+		@location = Location.all
+
+		@pois = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7127837,-74.0059413&radius=500&types=points_of_interest&rankby=prominence&key=AIzaSyCw2wYoFzAnvsFrxkolKpuht5JkM3UeIZ0")
+
 	end
 
 	def new
