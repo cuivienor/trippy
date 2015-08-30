8d04fc191ec6b4513356003c75aa50ad50b32227
Peter Petrov <petrov.p.peter@gmail.com>
2015-08-30 00:01:45 -0400

    create user form works!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_and_belongs_to_many :pois
   has_many :trips
   has_many :locations, through: :pois
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :users
+  
+  get '/sessions/new', to: 'sessions#new', as: :new_sessions
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
