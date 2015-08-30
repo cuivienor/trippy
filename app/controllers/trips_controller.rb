0c7563464fc499c40e4cd48afb33b524e735a655
Johnathan Chei <j.chei175@gmail.com>
2015-08-30 15:38:01 -0400

    more additions to trip controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -15,7 +15,40 @@ class TripsController < ApplicationController
 		#for multiple waypoints, save all into an array, then split and join with "|"
 		response = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{start}&destination=#{ending}&mode=walking&waypoints=optimize=true|#{way points, pipes between each}&sensor=false&key=#{api}")
 
-		
+				
+		# Legs
+			puts "*****************    LEGS OF JOURNEY    ****************"
+				# From location to to location
+				puts google.routes[0].legs
+
+		# Steps
+			puts "*****************    STEPS    ****************"
+				# overview
+				puts google.routes[0].legs[0].steps
+
+			# the following is for each individual step in the leg
+			puts "*****************    START    ****************"
+				# starting lat & long
+				puts "lat: "+google.routes[0].legs[0].steps[0].start_location.lat
+				puts "long: "+google.routes[0].legs[0].steps[0].start_location.lng
+
+			puts "*****************    END    ****************"
+				# ending lat & long
+				puts "lat: "+google.routes[0].legs[0].steps[0].end_location.lat
+				puts "long: "+google.routes[0].legs[0].steps[0].end_location.lng
+
+			puts "*****************    DISTANCE    ****************"
+				# total distance 
+				puts "distance: "+google.routes[0].legs[0].steps[0].distance.text
+
+			puts "*****************    DURATION    ****************"
+				# total time duration
+				puts "duration: "+google.routes[0].legs[0].steps[0].duration.text
+
+			puts "*****************    DIRECTIONS    ****************"
+				# directions (note: part of directions have 'bold' tags for html use)
+				puts "direction: "+google.routes[0].legs[0].steps[0].html_instructions
+			
 	end
 
 	# Saves the info from results page
