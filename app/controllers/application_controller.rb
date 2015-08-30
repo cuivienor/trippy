35e02e04503b9b15f0361531d714a1079faa9c9a
Peter Petrov <petrov.p.peter@gmail.com>
2015-08-30 11:58:13 -0400

    added some comments and helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  # user auth helpers
+  def logged_in?
+    session[:user_id]
+  end
+
+  def current_user?
+    session[:user_id] == params[:user_id].to_i
+  end
+
+  def authorized?
+    logged_in? && current_user?
+  end
+
+  
+  
 end
