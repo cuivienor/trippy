49c7e617105582090b4068e3f69565be7a061ee9
James Leisy <jamesleisy@gmail.com>
2015-08-30 17:10:16 -0400

    poi controllers and views

--- app/controllers/pois_controller.rb
+++ app/controllers/pois_controller.rb
@@ -1,10 +1,15 @@
+require 'google'
+
 class PoisController < ApplicationController
 
+include Google
+
 	def index
-		@pois = Poi.all
+		#@pois = Poi.all
 		@location = Location.all
 
-		@pois = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=40.7127837,-74.0059413&radius=500&types=points_of_interest&rankby=prominence&key=AIzaSyCw2wYoFzAnvsFrxkolKpuht5JkM3UeIZ0")
+		@map = getPois("40.7127837,-74.0059413")
+
 
 	end
 
--- lib/google.rb
+++ lib/google.rb
@@ -6,6 +6,7 @@ module Google
 
   TextSearchBase = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
   APIKEY = ENV['GOOGLEAPIKEY']
+  PoiSearch = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
   
   def getLocation(name)
     query = URI.encode_www_form('query' => name, 'key' => APIKEY)
@@ -14,7 +15,15 @@ module Google
     # parse response to have only place_id, lat,long, name
   end
 
-  def getPois(place_id)
+  def getPois(latlong)
+      poi = 'points_of_interest'
+      rank = 'prominence'
+      radius = 500
+
+    query = URI.encode_www_form('location' => latlong, 'types' => poi, 'radius' => radius, 'rankby' => rank, 'key' => APIKEY)
+    link = PoiSearch + query
+    response = HTTParty.get(link)["results"]
+
     #return array of POIS [{place_id, name}]
   end
 
