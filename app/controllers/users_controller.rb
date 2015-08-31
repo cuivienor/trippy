d4d1e5680164b95d2616520e9dd3f6ff94cb7662
Johnathan Chei <j.chei175@gmail.com>
2015-08-31 19:55:44 -0400

    form validation for new user is working! left comments to validate checkboxes in poi and location

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,15 @@ class UsersController < ApplicationController
 
   def create
     # TODO Add validation for duplicate things!
-    User.create(user_params)
-    flash[:notice] = "User Successfully Created"
-    flash[:username] = user_params[:username]
-    redirect_to sessions_path
+    @user = User.create(user_params)
+    if @user.save
+      flash[:notice] = "User Successfully Created"
+      flash[:username] = user_params[:username]
+      redirect_to sessions_path
+    else
+      render new_user_path
+    end
+
   end
 
   def show
@@ -25,7 +30,7 @@ class UsersController < ApplicationController
   def user_params
     params
       .require(:user)
-      .permit(:username, :email, :first_name, :first_name, :last_name, :password)
+      .permit(:username, :email, :first_name, :first_name, :last_name, :password, :password_confirmation)
   end
   
 end
--- app/models/poi.rb
+++ app/models/poi.rb
@@ -3,4 +3,5 @@ class Poi < ActiveRecord::Base
   has_and_belongs_to_many :trips
   belongs_to :location
   validates :google_place, uniqueness: true
+  # add validation for checked boxes
 end
