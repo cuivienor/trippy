07dec4b5d70847138c9e50eb04bfa7708f0d3372
Peter Petrov <petrov.p.peter@gmail.com>
2015-09-01 17:13:20 -0400

    migrate database to have pois images

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150831235114) do
+ActiveRecord::Schema.define(version: 20150901210954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150831235114) do
     t.string  "google_place"
     t.string  "latlong"
     t.integer "location_id"
+    t.text    "img_url"
   end
 
   add_index "pois", ["location_id"], name: "index_pois_on_location_id", using: :btree
