567eef0d3c054e02a3d8891b08008d609fb37e21
Peter Petrov <petrov.p.peter@gmail.com>
2015-09-01 14:34:20 -0400

    fixed uniq locations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_and_belongs_to_many :pois
   has_many :trips
-  has_many :locations, through: :pois
+  has_many :locations, -> {distinct}, through: :pois
   validates :username, presence: true, uniqueness: true
   validates :email, :first_name, :last_name, presence: true
   validates :password, length: { minimum: 7 }
