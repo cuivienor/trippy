a86f2cd23773f91e1fac73124a8d7d65d73061ef
David Vinkesteijn <dvinkesteijn@gmail.com>
2015-09-01 09:37:00 -0400

    getTickets, getImage in google.rb | Gemfiles have been changed because of the rest-client gem

--- lib/google.rb
+++ lib/google.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'rest-client'
 
 module Google
 
@@ -7,9 +8,12 @@ module Google
   TextSearchBase = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
   TextDirectionBase = "https://maps.googleapis.com/maps/api/directions/json?"
   TextDetailsBase = "https://maps.googleapis.com/maps/api/place/details/json?"
-  APIKEY = ENV['GOOGLEAPIKEY']
   PoiSearch = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
-  
+  AirportCodeBase = "http://iatacodes.org/api/v1/cities?"
+  PhotoBase = "https://maps.googleapis.com/maps/api/place/photo?"
+  APIKEY = ENV['GOOGLEAPIKEY']
+  FLIGHTKEY = ENV['IATAKEY']
+
   def getLocation(name)
     query = URI.encode_www_form('query' => name, 'key' => APIKEY)
     link = TextSearchBase + query
@@ -58,17 +62,54 @@ module Google
     link = TextDetailsBase + query
     response = HTTParty.get(link)
     name = response["result"]["name"]
+    photoreference = response["result"]["photos"]["photo_reference"]
     google_loc = response["result"]["geometry"]["location"]["lat"].to_s + "," + response["result"]["geometry"]["location"]["lng"].to_s
     details_params = {name: name, latlong: google_loc, google_place: place_id}
   end
   
-
   def getStart(address)
     query = query = URI.encode_www_form('query' => address, 'key' => APIKEY)
     link = TextSearchBase + query
     response = HTTParty.get(link)
     place_id = response["results"][0]["place_id"]
+  end
 
+  def getTickets(city)
+    # TBD -> dates, dynamic origin, more pricing e.g 5 options 
+    query = URI.encode_www_form('api_key' => FLIGHTKEY)
+    link = AirportCodeBase + query
+    response = HTTParty.get(link)
+    destination = response["response"].find{|a| a["name"] == city.titleize}["code"]
+    response = RestClient.post 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=' + APIKEY,
+             {
+               request: {
+                 passengers: {
+                   adultCount: 1
+                 },
+                 slice: [
+                   {
+                     origin: "JFK",
+                     destination: destination,
+                     date: "2015-10-14"
+                   },
+                   {
+                     origin: destination,
+                     destination: "JFK",
+                     date: "2015-11-14"
+                   }
+                 ]
+               }
+             }.to_json,
+             :content_type => :json
+    ticket = JSON.parse(response.body)
+    pricing1 = ticket["trips"]["tripOption"][0]["pricing"][0]["saleTotal"]
+    binding.pry
+  end
+
+  def getImage(photoreference)
+    # photoreference from output getDetails, set maxwidth to preferred size
+    query = URI.encode_www_form('maxwidth' => "400", "photoreference" => photoreference, 'key' => APIKEY)
+    img_url = PhotoBase + query
   end
 
 end
