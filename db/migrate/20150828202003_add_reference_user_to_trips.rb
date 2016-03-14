a524a5531e79f07b43b3d33c7a8fdb0f85dcc321
Peter Petrov <petrov.p.peter@gmail.com>
2016-03-14 11:57:22 -0400

    Fixed a db error

--- db/migrate/20150828202003_add_reference_user_to_trips.rb
+++ db/migrate/20150828202003_add_reference_user_to_trips.rb
@@ -1,5 +1,5 @@
 class AddReferenceUserToTrips < ActiveRecord::Migration
   def change
-  	add_reference :trips, :user, index: true, foreign_key: true
+    add_reference :trips, :user, index: true, foreign_key: true
   end
 end
